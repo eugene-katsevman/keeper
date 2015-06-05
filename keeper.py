605c9577c5e4d06dac2a90774ee25a1064ed11bd
Eugene Katsevman <eugene.katsevman@gmail.com>
2015-06-05 03:55:43 +0300

    обработка книг, правильная обработка параметра list, обработка доп. тегов, случайные задачи

--- keeper.py
+++ keeper.py
@@ -5,6 +5,8 @@ import tasks
 import sys
 import settings
 import argparse
+import random
+from settings import *
 from datetime import datetime
 
 taskpool = tasks.load_all()
@@ -17,14 +19,16 @@ def scheduled(arg):
 
 def list_topic(arg):
     task_list = [task for task in taskpool.tasks if not args.topic or set(task.topics).intersection(set(arg.topic))]
-    if args.topic:
-        task_list += [task for task in taskpool.special_tasks if not args.topic or set(task.topics).intersection(set(arg.topic))]
+    if arg.topic:
+        if set(arg.topic).intersection(set(IGNORED_SECTIONS)):
+            print "in ignored"
+            task_list += [task for task in taskpool.special_tasks if not args.topic or set(task.topics).intersection(set(arg.topic))]
     if args.unscheduled:
         task_list = [task for task in task_list if not task.upper_limit]
     for task in task_list:
         print task
     if not args.no_total:
-        print "Total: ", sum([task.length for task in task_list if task.length])
+        print "Total: ", len(task_list), "task(s), ", sum([task.length for task in task_list if task.length]), "h of worktime"
 
 def today(arg):
     for task in taskpool.today():
@@ -35,6 +39,16 @@ def test(arg):
     for task in task_list:
         print task
         print task.generate_timespanset(datetime(2015, 5, 1), datetime(2015, 5, 9))
+
+def random_tasks(arg):
+    task_list = [task for task in taskpool.tasks if not task.periodics and not task.upper_limit]
+    
+    sample = random.sample(task_list, 10)
+    for task in sample:
+        print task
+    if not args.no_total:
+        print "Total: ", sum([task.length for task in sample if task.length]), "h of worktime"
+        
 
 parser = argparse.ArgumentParser(description='console timekeeper')
 subparsers = parser.add_subparsers()
@@ -56,6 +70,11 @@ parser_today.set_defaults(func=today)
 
 parser_test = subparsers.add_parser('test', help='Testy test')
 parser_test.set_defaults(func=test)
+
+parser_random = subparsers.add_parser('random', help='Show ten random tasks')
+parser_random.add_argument("--no-total", action="store_true", help = "do not count total work hours")
+
+parser_random.set_defaults(func=random_tasks)
 
 if (len(sys.argv) < 2):
     args = parser.parse_args(['check'])

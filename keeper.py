26bdcecd9e84fc8a8f626036918126cbda2a866f
Eugene Katsevman <eugene.katsevman@gmail.com>
2017-02-12 00:31:41 +0300

    gedit to xed

--- keeper.py
+++ keeper.py
@@ -119,9 +119,9 @@ def random_tasks(arg):
 
 def edit(arg):
     if args.filenames:
-        os.system("gedit "+" ".join([tasks.get_dir()+fn + ".todo" for fn in args.filenames]))
+        os.system("xed "+" ".join([tasks.get_dir()+fn + ".todo" for fn in args.filenames]))
     else:
-        os.system("todo")
+        os.system("xed "+tasks.get_dir()+"*.todo")
 
 
 def show_topics(arg):
